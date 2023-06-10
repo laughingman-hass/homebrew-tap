758aca7b6f63ae5516911d653ccd64ccff3e8847
Hass Khalid <me@razgriz.io>
2023-06-10 08:07:18 +0100

    checksum added to muse-hub

--- Casks/muse-hub.rb
+++ Casks/muse-hub.rb
@@ -1,6 +1,6 @@
 cask "muse-hub" do
   version "4.0.2"
-  sha256 ""
+  sha256 "e01052ec3042f5c8e193f0f671d7eb9bc90b47d01d1fef8c6b6c1b306b1df25d"
 
   url "https://pub-c7a32e5b5d834ec9aeef400105452a42.r2.dev/Muse_Hub.dmg"
 
