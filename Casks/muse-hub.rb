04be011853aae896d4fcd06d5e5a49f31ee6bfb1
Hass Khalid <me@razgriz.io>
2023-06-10 08:06:17 +0100

    moved muse-hub to casks dir

--- Casks/muse-hub.rb
+++ Casks/muse-hub.rb
@@ -1,9 +1,11 @@
 cask "muse-hub" do
   version "4.0.2"
-  sha256 "a3738d0513d736918a6d71535ef3d85dd184af267c05698e49ac4c6b48f38e17"
+  sha256 ""
 
   url "https://pub-c7a32e5b5d834ec9aeef400105452a42.r2.dev/Muse_Hub.dmg"
+
   name "Muse Hub"
+
   desc "Open-source music notation software"
   homepage "https://musescore.org/en"
 
